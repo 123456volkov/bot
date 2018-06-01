c5f5b2c470d12121ffc516e45abb6f326cc5f2b2
lucii7vel <lucii7vel@gmail.com>
2018-06-02 00:32:27 +0300

    как-то так это работает

--- pyBot.py
+++ pyBot.py
@@ -1,1 +1,10 @@
-import pyTelgatamBotAPIs
+import telebot
+import misc
+
+bot = telebot.TeleBot(misc.token);
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Hey, I'm your english teacher")
+	
+bot.polling()
